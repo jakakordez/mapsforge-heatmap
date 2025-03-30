a1614721264b225d7e8204a7c93481ba0062e02b
Jaka Kordez <jaka.kordez@gmail.com>
2025-03-30 11:26:32 +0200

    Initial commit

---
+++ app/src/main/java/dev/is_a/jakakordez/mapsforge/heatmap/demo/MainActivity.kt
@@ -0,0 +1,47 @@
+package dev.is_a.jakakordez.mapsforge.heatmap.demo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.is_a.jakakordez.mapsforge.heatmap.demo.ui.theme.MapsforgeHeatmapTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MapsforgeHeatmapTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MapsforgeHeatmapTheme {
+        Greeting("Android")
+    }
+}
