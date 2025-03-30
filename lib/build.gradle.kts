c62dd9c04bf2bc9d31ba2dffc84c447d9e80a67e
Jaka Kordez <jaka.kordez@gmail.com>
2025-03-30 11:26:32 +0200

    Added mapsforge reference

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -33,7 +33,8 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.mapsforge)
+    implementation(libs.caverock)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
